8c830abc63adb3b6159b41f55838a477daa17e48
XinFul <mac14suarez@gmail.com>
2025-05-19 10:43:31 +0800

    frontend development

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,5 +1,5 @@
-import {Navigate, Outlet} from "react-router-dom";
-import { useStateContext } from "../contexts/ContextProvider";
+import {Link, Navigate, Outlet} from "react-router-dom";
+import {useStateContext} from "../contexts/ContextProvider";
 
 export default function DefaultLayout() {
     const {user, token} = useStateContext()
@@ -8,10 +8,29 @@ export default function DefaultLayout() {
         return <Navigate to="/login"/>
     }
 
+    const onLogout = (ev) => {
+        ev.preventDefault()
+    }
     return (
-        <div>
-            Default
-            <Outlet/>
+        <div id="defaultLayout">
+            <aside>
+                <Link to="/dashboard">Dashboard</Link>
+                <Link to="/users">Users</Link>
+            </aside>
+            <div className="content">
+                <header>
+                    <div>
+                        Header
+                    </div>
+                    <div>
+                        {user.name}
+                        <a href="#" className="btn-logout" onClick={onLogout}>Logout</a>
+                    </div>
+                </header>
+                <main>
+                    <Outlet/>
+                </main>
+            </div>
         </div>
     )
 }
